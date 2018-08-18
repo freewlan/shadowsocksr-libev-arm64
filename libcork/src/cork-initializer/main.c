4611d12edc12d933e4b304f60d5b54fc54e04578
railjty <335908093@qq.com>
2018-08-18 18:16:35 +0800

    second refix

---
+++ libcork/src/cork-initializer/main.c
@@ -0,0 +1,14 @@
+/* -*- coding: utf-8 -*-
+ * ----------------------------------------------------------------------
+ * Copyright © 2012, libcork authors
+ * All rights reserved.
+ *
+ * Please see the COPYING file in this distribution for license details.
+ * ----------------------------------------------------------------------
+ */
+
+int
+main(int argc, char **argv)
+{
+    return 0;
+}
