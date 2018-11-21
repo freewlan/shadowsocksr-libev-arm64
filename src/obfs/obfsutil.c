67219b347bd9f6015ff0563a3b153be38c7d1f3b
Akkariiin <Akkariin@protonmail.com>
2018-11-21 14:36:06 +0800

    fix g_endian_test

--- src/obfs/obfsutil.c
+++ src/obfs/obfsutil.c
@@ -4,6 +4,8 @@
 #include "obfsutil.h"
 #include "encrypt.h"
 
+static const uint32_t g_endian_test = 1;
+
 int get_head_size(char *plaindata, int size, int def_size) {
     if (plaindata == NULL || size < 2)
         return def_size;
